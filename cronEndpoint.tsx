487ec3264aa6cc574933d4fd825b6bce794ccd2d
Absterrg0 <Parvj5212@gmail.com>
2024-10-26 21:57:36 +0530

    Configured

--- cronEndpoint.tsx
+++ cronEndpoint.tsx
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Health check/keep-alive endpoint
-app.get('/keep-alive', (req, res) => {
+app.get('/', (req, res) => {
   res.send('WebSocket server is alive');
 });
 
